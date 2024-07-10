9340b2179e1a06b08e7d08bfe76c91d2ab26250b
Naim Azizi <naim.azizi@traveloka.com>
2024-07-10 16:04:50 +0700

    change font

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
-local font = 'Hack Nerd Font'
+local font = 'FiraCode Nerd Font'
 local font_size = platform().is_mac and 12 or 9
 
 return {
