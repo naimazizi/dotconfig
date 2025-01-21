b80b0df8ec7a1187377403d084ef1560a64e0182
Naim Azizi <naim.azizi@traveloka.com>
2025-01-21 12:16:08 +0700

    re-add image.lua & change molten backend to image.lua

--- nvim/lua/plugins/molten.lua
+++ nvim/lua/plugins/molten.lua
@@ -5,9 +5,47 @@ return {
     build = ":UpdateRemotePlugins",
     init = function()
       -- these are examples, not defaults. Please see the readme
-      vim.g.molten_image_provider = "wezterm"
+      vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
       vim.g.molten_auto_open_output = false
+      vim.keymap.set(
+        "n",
+        "<localleader>mi",
+        ":MoltenInit<CR>",
+        { silent = true, desc = "Molten - Initialize the plugin" }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>mw",
+        ":MoltenEvaluateOperator<CR>",
+        { silent = true, desc = "Molten - Run operator selection" }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>me",
+        ":MoltenEvaluateLine<CR>",
+        { silent = true, desc = "Molten - Evaluate line" }
+      )
+      vim.keymap.set(
+        "n",
+        "<localleader>mr",
+        ":MoltenReevaluateCell<CR>",
+        { silent = true, desc = "Molten - Re-evaluate cell" }
+      )
+      vim.keymap.set(
+        "v",
+        "<localleader>me",
+        ":<C-u>MoltenEvaluateVisual<CR>gv",
+        { silent = true, desc = "Molten - Evaluate visual selection" }
+      )
+      vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>", { silent = true, desc = "Molten - Delete cell" })
+      vim.keymap.set("n", "<localleader>mh", ":MoltenHideOutput<CR>", { silent = true, desc = "Molten - Hide output" })
+      vim.keymap.set(
+        "n",
+        "<localleader>ms",
+        ":noautocmd MoltenEnterOutput<CR>",
+        { silent = true, desc = "Molten - Show/enter output" }
+      )
     end,
   },
 }
