ad3ceb8ed371b9328619b892d42e7137d91710d6
Naim Azizi <naim.azizi@traveloka.com>
2025-02-17 03:54:07 +0700

    update theme & blink config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -38,8 +38,37 @@ else
             useLibraryCodeForTypes = true,
           },
         },
+        pylyzer = {
+          inlayHints = true,
+          checkOnType = false,
+          diagnostics = false,
+          smartCompletion = false,
+        },
       },
     }
     return ret
+  end
+  config = function()
+    require("blink.cmp").setup({
+      completion = {
+        menu = {
+          draw = {
+            -- We don't need label_description now because label and label_description are already
+            -- combined together in label by colorful-menu.nvim.
+            columns = { { "kind_icon" }, { "label", gap = 1 } },
+            components = {
+              label = {
+                text = function(ctx)
+                  return require("colorful-menu").blink_components_text(ctx)
+                end,
+                highlight = function(ctx)
+                  return require("colorful-menu").blink_components_highlight(ctx)
+                end,
+              },
+            },
+          },
+        },
+      },
+    })
   end
 end
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -3,6 +3,21 @@ return {
     "thesimonho/kanagawa-paper.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      undercurl = true,
+      transparent = true,
+      gutter = true,
+      dimInactive = true, -- disabled when transparent
+      terminalColors = true,
+      commentStyle = { italic = true },
+      functionStyle = { italic = true },
+      keywordStyle = { italic = false, bold = false },
+      statementStyle = { italic = false, bold = true },
+      typeStyle = { italic = true },
+      colors = { theme = {}, palette = {} }, -- override default palette and theme colors
+      overrides = function() -- override highlight groups
+        return {}
+      end,
+    },
   },
 }
