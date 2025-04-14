a8e2e39330fffb936731554bd23c474ea6a39d41
Naim Azizi <naim.azizi@traveloka.com>
2025-04-14 07:58:57 +0700

    tidy up lspconfig

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -15,64 +15,8 @@ else
   end
 
   vim.g.lazyvim_picker = "snacks"
+
   -- python config
   -- vim.g.lazyvim_python_lsp = "basedpyright" -- Use pyright for the time being, basedpyright is slow
   vim.g.lazyvim_python_ruff = "ruff"
-
-  opts = function()
-    ---@class PluginLspOpts
-    local ret = {
-      ---@type lspconfig.options
-      servers = {
-        basedpyright = {
-          typeCheckingMode = "strict",
-          single_file_support = true,
-          analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = "openFilesOnly",
-            useLibraryCodeForTypes = true,
-          },
-        },
-        pyright = {
-          typeCheckingMode = "strict",
-          single_file_support = true,
-          analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = "openFilesOnly",
-            useLibraryCodeForTypes = true,
-          },
-        },
-        pylyzer = {
-          inlayHints = true,
-          checkOnType = false,
-          diagnostics = false,
-          smartCompletion = false,
-        },
-      },
-    }
-    return ret
-  end
-  config = function()
-    require("blink.cmp").setup({
-      completion = {
-        menu = {
-          draw = {
-            -- We don't need label_description now because label and label_description are already
-            -- combined together in label by colorful-menu.nvim.
-            columns = { { "kind_icon" }, { "label", gap = 1 } },
-            components = {
-              label = {
-                text = function(ctx)
-                  return require("colorful-menu").blink_components_text(ctx)
-                end,
-                highlight = function(ctx)
-                  return require("colorful-menu").blink_components_highlight(ctx)
-                end,
-              },
-            },
-          },
-        },
-      },
-    })
-  end
 end
