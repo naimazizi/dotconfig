61f6cdedf58c37ef3d584d713fc697598267520d
Naim Azizi <naim.azizi@traveloka.com>
2025-04-20 20:23:32 +0700

    vscode plugin update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,124 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local function callVSCodeFunction(vsCodeCommand)
+  vim.cmd(vsCodeCommand)
+end
+
+local function vscodeMappings()
+  map("n", "<leader>cs", function()
+    print("go to symbols in editor")
+    callVSCodeFunction("call VSCodeCall('workbench.action.gotoSymbol')")
+  end, { noremap = true, silent = true, desc = "go to symbols in editor" })
+
+  map("n", "<S-l>", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.nextEditor')")
+  end, { noremap = true, desc = "switch between editor to next" })
+
+  map("n", "<S-h>", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.previousEditor')")
+  end, { noremap = true, desc = "switch between editor to previous" })
+
+  map("n", "gr", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.referenceSearch.trigger')")
+  end, { noremap = true, desc = "peek references inside vs code" })
+
+  map("n", "gd", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.revealDefinition')")
+  end, { noremap = true, desc = "go to definition inside vs code" })
+
+  map("n", "gD", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.revealDeclaration')")
+  end, { noremap = true, desc = "go to declaration inside vs code" })
+
+  map("n", "gI", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.peekImplementation')")
+  end, { noremap = true, desc = "peek Implementation inside vs code" })
+
+  map("n", "gy", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.goToTypeDefinition')")
+  end, { noremap = true, desc = "go to type definition inside vs code" })
+
+  map("n", "<leader>sd", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.problems.focus')")
+  end, { noremap = true, desc = "open problems and errors infos" })
+
+  map("n", "<leader>e", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+  end, { noremap = true, desc = "focus to file explorer" })
+
+  map("n", "<leader>fe", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+  end, { noremap = true, desc = "focus to file explorer" })
+
+  map("n", "<leader>ff", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.action.quickOpen')")
+  end, { noremap = true, desc = "open files" })
+
+  map("n", "<leader>gg", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.view.scm')")
+  end, { noremap = true, desc = "open git source control" })
+
+  map("n", "<leader>sml", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.list')")
+  end, { noremap = true, desc = "open bookmarks list for current files" })
+
+  map("n", "<leader>smL", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.listFromAllFiles')")
+  end, { noremap = true, desc = "open bookmarks list for all files" })
+
+  map("n", "<leader>smm", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.toggle')")
+  end, { noremap = true, desc = "toggle bookmarks" })
+
+  map("n", "<leader>smd", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.clear')")
+  end, { noremap = true, desc = "clear bookmarks from current file" })
+
+  map("n", "<leader>smr", function()
+    callVSCodeFunction("call VSCodeNotify('bookmarks.clearFromAllFiles')")
+  end, { noremap = true, desc = "clear bookmarks from all file" })
+
+  map("n", "<leader>cr", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.rename')")
+  end, { noremap = true, desc = "rename symbol" })
+
+  map("n", "<leader>ca", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.quickFix')")
+  end, { noremap = true, desc = "open quick fix in vs code" })
+
+  map("n", "<leader>cA", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.sourceAction')")
+  end, { noremap = true, desc = "open source Action in vs code" })
+
+  map("n", "<leader>cp", function()
+    callVSCodeFunction("call VSCodeNotify('workbench.panel.markers.view.focus')")
+  end, { noremap = true, desc = "open problems diagnostics" })
+
+  map("n", "<leader>cd", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.marker.next')")
+  end, { noremap = true, desc = "open problems diagnostics" })
+
+  map("n", "<leader>co", function()
+    callVSCodeFunction("call VSCodeNotify('editor.action.organizeImports')")
+  end, { noremap = true, desc = "organize import" })
+end
+
+if vim.g.vscode then
+  print("⚡connected to neovim!💯‼️🤗😎")
+  vscodeMappings()
+end
--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -1,4 +1,64 @@
+-- Configurations when using vscode-neovim.
+-- Modified from https://github.com/LazyVim/LazyVim/raw/main/lua/lazyvim/plugins/extras/vscode.lua
+
+if not vim.g.vscode then
+  return {}
+end
+
+local enabled = {
+  "lazy.nvim",
+  "flit.nvim",
+  "leap.nvim",
+  "mini.ai",
+  "mini.comment",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "nvim-ts-rainbow2", -- not used but required for loading treesitter
+  "vim-repeat",
+  "dial.nvim",
+  "LazyVim",
+}
+
+local Config = require("lazy.core.config")
+local Plugin = require("lazy.core.plugin")
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+
+-- HACK: disable all plugins except the ones we want
+local fix_disabled = Plugin.Spec.fix_disabled
+function Plugin.Spec.fix_disabled(self)
+  for _, plugin in pairs(self.plugins) do
+    if not (vim.tbl_contains(enabled, plugin.name) or plugin.vscode) then
+      plugin.enabled = false
+    end
+  end
+  fix_disabled(self)
+end
+
+-- HACK: don't clean plugins in vscode
+local update_state = Plugin.update_state
+function Plugin.update_state()
+  update_state()
+  Config.to_clean = {}
+end
+
 return {
+  {
+    "LazyVim/LazyVim",
+    config = function(_, opts)
+      opts = opts or {}
+      -- disable the colorscheme
+      opts.colorscheme = function() end
+      require("lazyvim").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { highlight = { enable = false }, rainbow = { enable = false } },
+  },
   {
     "vscode-neovim/vscode-multi-cursor.nvim",
     event = "VeryLazy",
