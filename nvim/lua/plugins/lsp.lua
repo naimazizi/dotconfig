d9827990255b5c8becb8e11f3d64848d711ef2cd
Naim Azizi <naim.azizi@traveloka.com>
2025-05-07 20:47:20 +0700

    update dashboard & lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
       "jmbuhr/cmp-pandoc-references",
       "xzbdmw/colorful-menu.nvim",
       "fang2hou/blink-copilot",
+      "copilotlsp-nvim/copilot-lsp",
     },
     event = "InsertEnter",
     opts = {
@@ -111,24 +112,27 @@ return {
       },
 
       keymap = {
-        preset = "super-tab",
-
         -- preset = "super-tab",
-        -- ["<Tab>"] = {
-        --   function(cmp)
-        --     if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-        --       cmp.hide()
-        --       return require("copilot-lsp.nes").apply_pending_nes()
-        --     end
-        --     if cmp.snippet_active() then
-        --       return cmp.accept()
-        --     else
-        --       return cmp.select_and_accept()
-        --     end
-        --   end,
-        --   "snippet_forward",
-        --   "fallback",
-        -- },
+
+        preset = "super-tab",
+        ["<Tab>"] = {
+          function(cmp)
+            if vim.b[vim.api.nvim_get_current_buf()].nes_state then
+              cmp.hide()
+              return (
+                require("copilot-lsp.nes").apply_pending_nes()
+                and require("copilot-lsp.nes").walk_cursor_end_edit()
+              )
+            end
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
       },
     },
   },
