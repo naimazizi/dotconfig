d8935f6ffcf4fb177a932ab9ff48316f04d13ac4
Naim Azizi <naim.azizi@traveloka.com>
2025-05-24 03:32:09 +0700

    fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,15 +9,6 @@ return {
     opts = {},
   },
   {
-    "saghen/blink.compat",
-    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-    version = "*",
-    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-    lazy = true,
-    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-    opts = {},
-  },
-  {
     "saghen/blink.cmp",
     version = not vim.g.lazyvim_blink_main and "*",
     build = vim.g.lazyvim_blink_main and "cargo build --release",
@@ -144,11 +135,6 @@ return {
           copilot = {
             name = "copilot",
             module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-            opts = {
-              max_completions = 3, -- Override global max_completions
-            },
           },
           avante = {
             module = "blink-cmp-avante",
@@ -164,8 +150,6 @@ return {
       },
 
       keymap = {
-        -- preset = "super-tab",
-
         preset = "super-tab",
         ["<Tab>"] = {
           function(cmp)
