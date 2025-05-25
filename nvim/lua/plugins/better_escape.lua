7572be1c6f77f6a17d606c399c1e26eabbd8eb63
Naim Azizi <naim.azizi@traveloka.com>
2025-05-25 14:04:36 +0700

    add better escape

---
+++ nvim/lua/plugins/better_escape.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "max397574/better-escape.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("better_escape").setup({
+        timeout = 100, -- time in milliseconds to wait for a second key press
+      })
+    end,
+  },
+}
