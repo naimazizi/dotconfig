df965445d985ffb17ffe3f1d170a8ccfa07bc2ac
Naim Azizi <naim.azizi@traveloka.com>
2025-06-07 11:09:04 +0700

    remove blink pairs

--- wezterm/config/bindings.lua
+++ wezterm/config/bindings.lua
@@ -110,7 +110,7 @@ local keys = {
       action = act.ActivateKeyTable({
          name = 'resize_font',
          one_shot = false,
-         timemout_miliseconds = 1000,
+         timeout_milliseconds = 1000,
       }),
    },
    -- resize panes
@@ -120,7 +120,7 @@ local keys = {
       action = act.ActivateKeyTable({
          name = 'resize_pane',
          one_shot = false,
-         timemout_miliseconds = 1000,
+         timeout_milliseconds = 1000,
       }),
    },
 }
