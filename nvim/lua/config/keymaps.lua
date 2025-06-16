2d6d399af1c00637570e72bd7984abff9c585429
Naim Azizi <naim.azizi@traveloka.com>
2025-06-16 11:25:16 +0700

    update neovide config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -368,6 +368,13 @@ if vim.g.vscode then
       .. vim.version().build
   )
   vscodeMappings()
+elseif vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 else
   -- Clear search and stop snippet on escape
   vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
