f7e2df9230c7454d5945d89d61114cdce828dd1c
Naim Azizi <naim.azizi@traveloka.com>
2025-08-04 08:23:08 +0700

    Add overlook peek & remove nonels

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -4,4 +4,32 @@ return {
     event = "BufRead",
     vscode = false,
   },
+  {
+    "WilliamHsieh/overlook.nvim",
+    event = "BufRead",
+    vscode = false,
+    keys = {
+      {
+        "go",
+        function()
+          require("overlook.api").peek_definition()
+        end,
+        desc = "Overlook: Peek definition",
+      },
+      {
+        "gL",
+        function()
+          require("overlook.api").close_all()
+        end,
+        desc = "Overlook: Close all popup",
+      },
+      {
+        "gl",
+        function()
+          require("overlook.api").restore_popup()
+        end,
+        desc = "Overlook: Restore popup",
+      },
+    },
+  },
 }
--- nvim/lua/plugins/lang/lua.lua
+++ nvim/lua/plugins/lang/lua.lua
@@ -2,6 +2,6 @@ vim.lsp.enable({ "emmylua_ls" })
 return {
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "emmylua_ls" } },
+    opts = { ensure_installed = { "emmylua_ls" }, automatic_installation = { enabled = true, exclude = { "lua_ls" } } },
   },
 }
