13844f5147b6fc704f7ba684c6280088d8706184
Naim Aryudya Azizi <naim.azizi@traveloka.com>
2025-11-24 10:41:01 +0700

    Update file picker back to fzf-lua

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -2,6 +2,19 @@ return {
   {
     "ibhagwan/fzf-lua",
     vscode = false,
+    dependencies = {
+      {
+        "elanmed/fzf-lua-frecency.nvim",
+        config = function()
+          require("fzf-lua-frecency").setup({
+            display_score = false,
+            cwd_only = true,
+            all_files = true,
+          })
+        end,
+      },
+      "folke/snacks.nvim",
+    },
     opts = {
       { "telescope" },
       previewers = {},
@@ -10,53 +23,8 @@ return {
       { "<leader>dd", "<cmd>:FzfLua dap_breakpoints<CR>", desc = "List Breakpoints" },
       { "<leader>dv", "<cmd>:FzfLua dap_variables<CR>", desc = "List Variables" },
       { "<leader>fp", "<cmd>:FzfLua zoxide<CR>", desc = "Projects" },
-      -- Disable keymap in favor of fff.nvim
-      { "<leader><space>", false },
-      { "<leader>ff", false },
-      { "<leader>fF", false },
-      { "<leader>fg", false },
-    },
-  },
-  {
-    "dmtrKovalenko/fff.nvim",
-    vscode = false,
-    build = function()
-      require("fff.download").download_or_build_binary()
-    end,
-    opts = {
-      prompt = " ",
-      title = "Find Files",
-    },
-    lazy = false,
-    keys = {
-      {
-        "<leader><space>",
-        function()
-          require("fff").find_files()
-        end,
-        desc = "Find Files",
-      },
-      {
-        "<leader>ff",
-        function()
-          require("fff").find_files()
-        end,
-        desc = "Find Files",
-      },
-      {
-        "<leader>fF",
-        function()
-          require("fff").find_files()
-        end,
-        desc = "Find Files (cwd)",
-      },
-      {
-        "<leader>fg",
-        function()
-          require("fff").find_in_git_root()
-        end,
-        desc = "Find Files (git-files)",
-      },
+      { "<leader>fr", "<cmd>:FzfLua frecency<CR>", desc = "Recent" },
+      { "<leader><localleader>", "<cmd>:FzfLua global<CR>", desc = "Global Search" },
     },
   },
 }
