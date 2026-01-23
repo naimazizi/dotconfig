e6bb46db29345082719c1c0e2c6900343f2f9d68
Naim Aryudya Azizi <naim.azizi@traveloka.com>
2026-01-23 23:27:28 +0700

    fix neovide

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -35,3 +35,8 @@ vim.g.sh_ft = { "sh", "bash", "zsh", "ksh" }
 vim.g.slime_target = "tmux"
 vim.g.slime_cell_delimiter = "# %%"
 vim.g.slime_bracketed_paste = 1
+
+if vim.g.neovide then
+  vim.env.PATH = "/opt/homebrew/bin:" .. vim.env.PATH
+  vim.env.PATH = "/home/linuxbrew/.linuxbrew/bin:" .. vim.env.PATH
+end
