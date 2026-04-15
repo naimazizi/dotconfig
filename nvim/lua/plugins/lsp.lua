4b11677623e54dcac68c8ad864135dd38ffa55c9
Naim Aryudya Azizi <naim.azizi@traveloka.com>
2026-04-15 14:01:19 +0700

    fix(nvim): fix typo-lsp does not respect typos.toml config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -161,15 +161,7 @@ return {
 
       -- Typos
       vim.lsp.config("typos_lsp", {
-        -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
-        cmd_env = { RUST_LOG = "error" },
         init_options = {
-          -- Custom config. Used together with a config file found in the workspace or its parents,
-          -- taking precedence for settings declared in both.
-          -- Equivalent to the typos `--config` cli argument.
-          config = "~/code/typos-lsp/crates/typos-lsp/tests/typos.toml",
-          -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
-          -- Defaults to error.
           diagnosticSeverity = "Hint",
         },
       })
