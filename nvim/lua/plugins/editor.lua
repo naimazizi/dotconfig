72e2e8c29f2749e96fb84ebe18fba537f32bd3e1
Naim Aryudya Azizi <naim.azizi@traveloka.com>
2026-06-14 21:53:04 +0700

    chore(nvim): add blink.pairs build function

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -2,9 +2,11 @@ return {
   {
     "saghen/blink.pairs",
     vscode = false,
-    event = "BufRead",
     version = "*",
     dependencies = "saghen/blink.download",
+    build = function()
+      require("blink.pairs").download():pwait(60000)
+    end,
     --- @module 'blink.pairs'
     --- @type blink.pairs.Config
     opts = {
