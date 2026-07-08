bfe40c66fe3f317be5ac0d7e397c7db064081731
Naim Aryudya Azizi <naim.azizi@traveloka.com>
2026-07-08 07:36:27 +0700

    fix(opencode): remove claude plugin to speed up startup

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -62,78 +62,8 @@ return {
     },
   },
   {
-    "carlos-algms/agentic.nvim",
-    event = "VeryLazy",
-    vscode = false,
-    --- @type agentic.PartialUserConfig
-    opts = {
-      provider = "pi-acp",
-      windows = {
-        position = "right",
-        width = "30%",
-      },
-      slash_commands = {
-        auto_trigger = false,
-      },
-      file_picker = {
-        auto_trigger = false,
-      },
-    },
-    keys = {
-      {
-        "<leader>aa",
-        function()
-          require("agentic").toggle()
-        end,
-        mode = { "n", "v" },
-        desc = "Toggle Agentic Chat",
-      },
-      {
-        "<leader>as",
-        function()
-          require("agentic").add_selection_or_file_to_context()
-        end,
-        mode = { "n", "v" },
-        desc = "Add file or selection to Agentic to Context",
-      },
-      {
-        "<leader>ai",
-        function()
-          require("agentic").new_session()
-        end,
-        mode = { "n", "v" },
-        desc = "New Agentic Session",
-      },
-      {
-        "<leader>ar", -- ai Restore
-        function()
-          require("agentic").restore_session()
-        end,
-        desc = "Agentic Restore session",
-        silent = true,
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ad", -- ai Diagnostics
-        function()
-          require("agentic").add_current_line_diagnostics()
-        end,
-        desc = "Add current line diagnostic to Agentic",
-        mode = { "n" },
-      },
-      {
-        "<leader>aD", -- ai all Diagnostics
-        function()
-          require("agentic").add_buffer_diagnostics()
-        end,
-        desc = "Add all buffer diagnostics to Agentic",
-        mode = { "n" },
-      },
-    },
-  },
-  {
     "sudo-tee/opencode.nvim",
-    enabled = false,
+    enabled = true,
     vscode = false,
     cmd = "Opencode",
     keys = {
